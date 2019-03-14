0ea869f59d6020e107df5e12748bac57b3df6c66
Kyle Workman <kyleworkman15@augustana.edu>
2019-03-14 14:03:39 -0500

    Created new project

---
+++ app/src/main/java/edu/augustana/osleventsandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.augustana.osleventsandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.osleventsandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
