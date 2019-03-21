8d73b7f926b4dc5f84e38d12c1a6a18dd37b6f8b
jhaeme15 <jaredhaeme15@augustana.edu>
2019-03-21 13:02:00 -0500

    Created page for single event info

---
+++ app/src/main/java/edu/augustana/osleventsandroid/SingleEventPage.java
@@ -0,0 +1,15 @@
+package edu.augustana.osleventsandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.osleventsandroid.R;
+
+public class SingleEventPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_event_page);
+    }
+}
